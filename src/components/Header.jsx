003edae6aea7b6f355b4aaff9438a04fcc36930a
GabrielOchieng <ogingagabriel@gmail.com>
2024-05-10 22:40:11 +0300

    responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,9 @@ const Header = () => {
       className="header  bg-cover bg-no-repeat bg-center h-96 text-center flex flex-col justify-center gap-3 relative"
       style={{ backgroundImage: `url(${backgroundImg.src})` }}
     >
-      <h1 className="text-white text-6xl font-bold ">DISCOVER YOUR NEW HOME</h1>
+      <h1 className="text-white text-2xl sm:text-4xl md:text-6xl font-bold ">
+        DISCOVER YOUR NEW HOME
+      </h1>
       <p className="text-white text-lg text-center font-bold">
         Helping Thousands of renters find their perfect home.
       </p>
@@ -40,7 +42,7 @@ const Header = () => {
           <input
             type="text"
             placeholder="Nairobi"
-            className="ml-3 outline-none p-3 h-12 rounded-sm border-green-400 border w-96 "
+            className="ml-3 outline-none p-3 h-12 rounded-sm border-green-400 border max-[390px]:w-56  w-64 sm:w-96 "
             value={searchTerm}
             onChange={handleSearchChange}
           />
